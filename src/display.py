5a8744aa46818f7008d7ba5832e8b71da9759bee
Pierre Jacquier <pierrejacquier39@gmail.com>
2020-01-21 10:52:15 +0000

    Added model predictions

--- src/display.py
+++ src/display.py
@@ -5,6 +5,7 @@ from rpilcdmenu.items import FunctionItem, SubmenuItem, MessageItem, MenuItem
 from rotaryencoder import RotaryEncoder
 from power import Power
 from servermodel import ServerModel
+from utils import get_serial
 
 RS_PIN = 27
 E_PIN = 22
@@ -16,79 +17,75 @@ class Display(object):
         self.temperature_sensor = temperature_sensor
         self.rotary_encoder = RotaryEncoder(self)
         self.power = Power(POWER_PIN)
-        self.model = ServerModel(1)
+        self.model = ServerModel(get_serial())
 
         self.boil_temps = [60, 65, 70, 75, 80, 85, 90, 95, 100]
         self.boil_volumes = [0.50, 0.75, 1.00, 1.25, 1.50, 1.75]
 
+        self.menu = RpiLCDMenu(RS_PIN, E_PIN, DB_PINS)
         self.init_menu()
 
     def init_menu(self):
-        self.menu = RpiLCDMenu(RS_PIN, E_PIN, DB_PINS)
         # Calibrate Submenu
-        read_temp_submenu = RpiLCDSubMenu(self.menu)
-        read_temp_submenu.append_item(
-                FunctionItem("Back", lambda: read_temp_submenu.exit()))
-        read_temp_submenu.append_item(MenuItem(self.getTemp()))
+        self.read_temp_submenu = RpiLCDSubMenu(self.menu)
+        self.read_temp_submenu.append_item(
+                FunctionItem("Back", lambda: self.read_temp_submenu.exit()))
+        self.read_temp_submenu.append_item(MenuItem(self.getTemp()))
 
         # Options Submenu
-        options_submenu = RpiLCDSubMenu(self.menu)
-        options_submenu.append_item(
+        self.options_submenu = RpiLCDSubMenu(self.menu)
+        self.options_submenu.append_item(
                 FunctionItem("Setup", lambda: print("setup")))
-        options_submenu.append_item(
-                SubmenuItem("Read temp", read_temp_submenu, options_submenu))
-        options_submenu.append_item(
-                FunctionItem("Back", lambda: options_submenu.exit()))
+        self.options_submenu.append_item(
+                SubmenuItem("Read temp", self.read_temp_submenu, self.options_submenu))
+        self.options_submenu.append_item(
+                FunctionItem("Back", lambda: self.options_submenu.exit()))
 
         # Select-temperature Submenu
-        boil_temp_submenu = RpiLCDSubMenu(self.menu)
+        self.boil_temp_submenu = RpiLCDSubMenu(self.menu)
         for t in self.boil_temps:
-            boil_message = FunctionItem(f"{t} C", self.onTempSelected, [t])
-            boil_temp_submenu.append_item(boil_message) 
-        boil_temp_submenu.append_item(
-                FunctionItem("Back", lambda: boil_temp_submenu.exit()))
+            boil_message = FunctionItem(f"{t} C", self.boilTo, [t])
+            self.boil_temp_submenu.append_item(boil_message) 
+        self.boil_temp_submenu.append_item(
+                FunctionItem("Back", lambda: self.boil_temp_submenu.exit()))
 
         # Select-volume Submenu
-        boil_vol_submenu = RpiLCDSubMenu(self.menu)
+        self.boil_vol_submenu = RpiLCDSubMenu(self.menu)
         for v in self.boil_volumes:
-            boil_vol_submenu.append_item(
-                    SubmenuItem(f"{v:.2f} L", boil_temp_submenu, boil_vol_submenu))
-        boil_vol_submenu.append_item(
-                FunctionItem("Back", lambda: boil_vol_submenu.exit()))
+            self.boil_vol_submenu.append_item(
+                    SubmenuItem(f"{v:.2f} L", self.boil_temp_submenu, self.boil_vol_submenu))
+        self.boil_vol_submenu.append_item(
+                FunctionItem("Back", lambda: self.boil_vol_submenu.exit()))
 
         # Select-volume Submenu
-        calib_vol_submenu = RpiLCDSubMenu(self.menu)
+        self.calib_vol_submenu = RpiLCDSubMenu(self.menu)
         for v in self.boil_volumes:
-            calib_vol_submenu.append_item(
+            self.calib_vol_submenu.append_item(
                     FunctionItem(f"{v:.2f} L", self.calibrate, [v]))
-        boil_vol_submenu.append_item(
-                FunctionItem("Back", lambda: calib_vol_submenu.exit()))
+        self.boil_vol_submenu.append_item(
+                FunctionItem("Back", lambda: self.calib_vol_submenu.exit()))
 
         # Main Menu
-        boil_item = SubmenuItem("Boil", boil_vol_submenu, self.menu)
-        temp_item = SubmenuItem("Calibrate", calib_vol_submenu, self.menu)
-        other_item = SubmenuItem("Options", options_submenu, self.menu)
+        boil_item = SubmenuItem("Boil", self.boil_vol_submenu, self.menu)
+        temp_item = SubmenuItem("Calibrate", self.calib_vol_submenu, self.menu)
+        other_item = SubmenuItem("Options", self.options_submenu, self.menu)
         self.menu.append_item(boil_item) \
                  .append_item(temp_item) \
                  .append_item(other_item)
 
         self.menu.start()
 
-    def onTempSelected(self, t):
-        print(f"Boiling to {t} C...")
+    def boilTo(self, t):
+        volume = self.boil_volumes[self.boil_vol_submenu.current_option]
+        time_pred = self.model.predict(volume, t).item()
+        print(f"{volume}L,{t}C: {time_pred:.1f}s")
         self.menu.clearDisplay()
-        self.menu.message(f"Boiling to {t} C...")
+        self.menu.message(f"{volume}L,{t}C: {time_pred:.1f}s")
         self.power.on()
-        go_on = True
-        while go_on:
-            temp = round(self.temperature_sensor.read(), 1)
-            print(f"Temp: {temp} C")
-            if temp > int(t):
-                go_on = False
-            time.sleep(1)
+        time.sleep(time_pred)
         self.power.off()
         self.menu.clearDisplay()
-        self.menu.message(f"Done! {temp} C")
+        self.menu.message(f"Done! {t} C")
 
     def calibrate(self, volume=1.5):
         target_temp = 99
@@ -110,21 +107,26 @@ class Display(object):
                 go_on = False
             time.sleep(1)
         self.power.off()
-        self.menu.clearDisplay()
         self.menu.message(f"Done! Sending data to the server…")
         self.model.train(volume, temps, times)
         self.menu.message(f"Model updated!")
         print("Done training")
         time.sleep(1)
+        self.menu.clearDisplay()
+        self.calib_vol_submenu.exit()
+        self.menu.exit()
         self.init_menu()
 
     def onRotaryNext(self):
+        print("next")
         self.menu.processDown()
 
     def onRotaryPrevious(self):
+        print("prev")
         self.menu.processUp()
 
     def onRotaryPressed(self):
+        print("pressed")
         self.menu = self.menu.processEnter()
 
     def getTemp(self):
